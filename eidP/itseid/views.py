762c805a8af34b06df0f12384127bf0bb2349e22
siudro <sedra.abdulghaffar@gmail.com>
2022-07-19 15:51:44 +0300

    add holidays checker

--- eidP/itseid/views.py
+++ eidP/itseid/views.py
@@ -1,11 +1,16 @@
+from datetime import date
 from django.shortcuts import render
 import datetime
+from datetime import date
+import holidays
+
 # Create your views here.
 def index(request):
+    sa_holidays = holidays.SA()
+    sa_holidays = holidays.country_holidays('SA')  # this is a dict
     now = datetime.datetime.now()
     return render(request , "itseid/index.html", {
-        "eid":True
-         #now.month == 4 and now.day == 20
-         # to do:
-         # add https://github.com/dr-prodigy/python-holidays for accurate results 
-    })
+       "eid": date(now.year, now.month, now.day) in sa_holidays
+         #from https://github.com/dr-prodigy/python-holidays
+   })
+   
